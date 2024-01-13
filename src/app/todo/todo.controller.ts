7d1c10b6c0d9254f88b52de39d87eb6754301687
Ágatha <agathamagnusg@hotmail.com>
2024-01-13 10:23:20 -0300

    ✨ Add Swagger API documentation to TodoController

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -13,27 +13,33 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('todo')
+@ApiTags('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all todos' })
   async index() {
     return await this.todoService.findAll();
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a todo' })
   async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a todo' })
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneOrFail(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a todo' })
   async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() body: UpdateTodoDto,
@@ -42,6 +48,7 @@ export class TodoController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a todo' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
     await this.todoService.deleteById(id);
