1af8b6c167a5b385ebcad555de9d631285531ad1
Ágatha <agathamagnusg@hotmail.com>
2024-01-12 22:47:45 -0300

    ♻️ Refactor database configuration in app module to TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodoModule } from './app/todo/todo.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://hta:agatha@cluster0.r2eb6bu.mongodb.net/todo',
-    ),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', 'root'),
+        database: configService.get('DB_DATABASE', 'todo'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    TodoModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
