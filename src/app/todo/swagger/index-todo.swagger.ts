6f412373c6b53a7d20a06d88a4dcb780f2888f0b
Ágatha <agathamagnusg@hotmail.com>
2024-01-13 14:08:21 -0300

    ♻️ Refactor IndexTodoSwagger class

--- src/app/todo/swagger/index-todo.swagger.ts
+++ src/app/todo/swagger/index-todo.swagger.ts
@@ -1,6 +1,3 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
 import { TodoEntity } from '../entity/todo.entity';
 
-export class IndexTodoSwagger extends PartialType(
-  OmitType(TodoEntity, ['createdAt', 'updatedAt', 'deletedAt']),
-) {}
+export class IndexTodoSwagger extends TodoEntity {}
