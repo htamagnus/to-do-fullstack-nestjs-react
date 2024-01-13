52a8434eff8863670744cf64cfa9734374ca58ff
htamagnus <agathamagnusg@hotmail.com>
2024-01-13 14:28:42 -0300

    ✅ Update database password in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { TodoModule } from './app/todo/todo.module';
         host: configService.get('DB_HOST', 'localhost'),
         port: Number(configService.get('DB_PORT', 3306)),
         username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get('DB_PASSWORD', 'root'),
+        password: configService.get('DB_PASSWORD', '1234'),
         database: configService.get('DB_DATABASE', 'todo'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
