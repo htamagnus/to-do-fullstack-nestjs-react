c3682bae30e727f90a0d3e49623af3a6600b7b42
Ágatha <agathamagnusg@hotmail.com>
2024-01-13 10:22:21 -0300

    ⚰️ Remove unnecessary properties from UpdateTodoDto

--- src/app/todo/dto/update-todo.dto.ts
+++ src/app/todo/dto/update-todo.dto.ts
@@ -1,10 +1,3 @@
-import { IsNotEmpty } from 'class-validator';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends CreateTodoDto {
-  @IsNotEmpty()
-  task: string;
-
-  @IsNotEmpty()
-  isDone: number;
-}
+export class UpdateTodoDto extends CreateTodoDto {}
