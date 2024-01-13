f82b5d5caa76f8bb12d0bdababbb669116f5d155
Ágatha <agathamagnusg@hotmail.com>
2024-01-12 21:22:55 -0300

    ➕ Add NestJS Mongoose and TypeORM dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://hta:agatha@cluster0.r2eb6bu.mongodb.net/todo',
+    ),
+  ],
 })
 export class AppModule {}
