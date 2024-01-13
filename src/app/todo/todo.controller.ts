99d9a0a9c1d3e42fe4c224fb418b761358c3faec
Ágatha <agathamagnusg@hotmail.com>
2024-01-13 10:30:14 -0300

    ✨ Add Swagger API response annotations to TodoController

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -13,7 +13,7 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('todo')
 @ApiTags('todos')
@@ -22,24 +22,36 @@ export class TodoController {
 
   @Get()
   @ApiOperation({ summary: 'Get all todos' })
+  @ApiResponse({ status: 200, description: 'Return all todos.' })
   async index() {
     return await this.todoService.findAll();
   }
 
   @Post()
   @ApiOperation({ summary: 'Create a todo' })
+  @ApiResponse({
+    status: 201,
+    description: 'The todo has been successfully created.',
+  })
   async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Get a todo' })
+  @ApiResponse({ status: 200, description: 'Return a todo.' })
+  @ApiResponse({ status: 404, description: 'Todo not found.' })
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneOrFail(id);
   }
 
   @Put(':id')
   @ApiOperation({ summary: 'Update a todo' })
+  @ApiResponse({
+    status: 200,
+    description: 'The todo has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Todo not found.' })
   async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() body: UpdateTodoDto,
@@ -49,6 +61,11 @@ export class TodoController {
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a todo' })
+  @ApiResponse({
+    status: 204,
+    description: 'The todo has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Todo not found.' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
     await this.todoService.deleteById(id);
