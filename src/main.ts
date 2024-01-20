c127563389619753d88f6e7f40e6a41c3132f17b
htamagnus <agathamagnusg@hotmail.com>
2024-01-20 13:31:24 -0300

    ⬆️ Update global pipes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
