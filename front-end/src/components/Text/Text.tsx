730b23f2da136b1373f22b19ac4c6ddfaedf4aba
Ágatha <agathamagnusg@hotmail.com>
2024-01-25 16:01:43 -0300

    ✨ Add space utility to Text component

--- front-end/src/components/Text/Text.tsx
+++ front-end/src/components/Text/Text.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
-import { typography, TypographyProps } from 'styled-system'
+import { typography, TypographyProps, SpaceProps, space } from 'styled-system'
 
-type TextProps = TypographyProps
+type TextProps = TypographyProps & SpaceProps
 
 export const Text = styled.p<TextProps>`
     font-family: 14px;
     color: #fff;
     ${typography}
+    ${space}
 `
