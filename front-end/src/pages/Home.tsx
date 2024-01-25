fb53170253fc50c8fbffc3b60e8e23e1d4c5d53d
Ágatha <agathamagnusg@hotmail.com>
2024-01-25 16:43:13 -0300

    👔 Add logic to tasks

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,13 +1,28 @@
-import { Input, Text, Button, Row, Column } from 'components'
+import { Input, Text, Button, Row, Column, List } from 'components'
+import { useState } from 'react'
 
 export const Home = () => {
+    const [taskName, setTaskName] = useState('');
+    const [tasks, setTasks] = useState<{label: string }[]>([])
+
+    const handleOKButton = () => {
+        if (!taskName) return;
+        setTasks(previous => {
+            const copy = [...previous];
+            copy.push({ label: taskName });
+            return copy;
+        })
+        setTaskName('');
+    }
+
     return (
-        <Column>
-            <Text fontWeight="bold">Tasks</Text>
-            <Row>
-            <Input placeholder="Enter a task name here.."/>
-            <Button>Ok</Button>
+        <Column width="600px" margin="0 auto">
+            <Text fontWeight="bold" fontSize="bodyLarge" my="10px" pl="10px">Tasks</Text>
+            <Row width="100%">
+            <Input flex={1} placeholder="Enter a task name here.." value={taskName}onChange={(e) => setTaskName(e.target.value)} />
+            <Button onClick={handleOKButton}>Ok</Button>
             </Row>
+            <List items={tasks} />
         </Column>
     )
 }
