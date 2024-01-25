02e808d032db547cb4e7f530191e56da6bf169ae
Ágatha <agathamagnusg@hotmail.com>
2024-01-25 16:41:42 -0300

    ✨ Add styleds-system

--- front-end/src/components/Column/Column.tsx
+++ front-end/src/components/Column/Column.tsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components'
-import { layout, LayoutProps, space, SpaceProps } from 'styled-system'
+import { layout, LayoutProps, space, SpaceProps, color, ColorProps, borderRadius, BorderRadiusProps, border, BorderProps } from 'styled-system'
 
-type ColumnProps = LayoutProps & SpaceProps;
+type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps;
 
 export const Column = styled.div<ColumnProps>`
     display: flex;
     flex-direction: column;
     ${layout}
     ${space}
+    ${color}
+    ${borderRadius}
+    ${border}
 `
