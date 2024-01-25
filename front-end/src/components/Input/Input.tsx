1c76ff937cc73134b944d29a9ae07212aaeff5c5
Ágatha <agathamagnusg@hotmail.com>
2024-01-25 15:42:05 -0300

    ✨ Add Input component

---
+++ front-end/src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+    background-color: transparent;
+    padding: 10px;
+    border-bottom: 2px solid #fff;
+    color: #fff;
+
+    &::placeholder {
+        color: rgba(0, 0, 0, 0.2);
+    }
+`
