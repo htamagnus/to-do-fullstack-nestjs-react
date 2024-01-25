a08ae5f5e0a92ebe55c9ddd4f195bad6aab1da81
Ágatha <agathamagnusg@hotmail.com>
2024-01-25 15:07:54 -0300

    ⬆️ Update imports and add Routes component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,10 @@
-import { Theme } from "./themes";
+import { Theme } from "themes";
+import { Routes } from "routes";
 
 export const App = () => {
   return (
     <Theme>
-
+      <Routes />
     </Theme>
   )
 };
