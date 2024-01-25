83145592d371e70a6946888dcb13acc7a298e3d8
Ágatha <agathamagnusg@hotmail.com>
2024-01-25 15:43:03 -0300

    💄Add GlobalStyles component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,10 @@
-import { Theme } from "themes";
+import { Theme, GlobalStyles } from "themes";
 import { Routes } from "routes";
 
 export const App = () => {
   return (
     <Theme>
+      <GlobalStyles />
       <Routes />
     </Theme>
   )
