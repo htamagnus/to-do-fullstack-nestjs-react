056e1e96657cd5b01e9feffbb5bbb6cfabebdee0
Ágatha <agathamagnusg@hotmail.com>
2024-02-03 11:11:59 -0300

    ✨ Add flexbox, background, and boxShadow props to Column component

--- front-end/src/components/Column/Column.tsx
+++ front-end/src/components/Column/Column.tsx
@@ -1,14 +1,19 @@
 import styled from 'styled-components'
-import { layout, LayoutProps, space, SpaceProps, color, ColorProps, borderRadius, BorderRadiusProps, border, BorderProps } from 'styled-system'
+import { layout, LayoutProps, space, SpaceProps, color, ColorProps, borderRadius, BorderRadiusProps, border, BorderProps, flexbox, FlexboxProps, background, BackgroundProps, boxShadow, BoxShadowProps } from 'styled-system'
 
-type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps;
+type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps & FlexboxProps & BackgroundProps & BoxShadowProps;
 
 export const Column = styled.div<ColumnProps>`
     display: flex;
     flex-direction: column;
+    align-items: center;
+    flex-wrap: nowrap;
+    ${flexbox}
     ${layout}
     ${space}
     ${color}
     ${borderRadius}
     ${border}
+    ${background}
+    ${boxShadow}
 `
