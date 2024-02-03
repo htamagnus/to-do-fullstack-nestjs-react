0c7c0c44e0a7fa1da36d374a4994d163f0af575a
Ágatha <agathamagnusg@hotmail.com>
2024-02-03 14:16:58 -0300

    ✨ Add color prop to Text component

--- front-end/src/components/Text/Text.tsx
+++ front-end/src/components/Text/Text.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
-import { typography, TypographyProps, SpaceProps, space } from 'styled-system'
+import { typography, TypographyProps, SpaceProps, space, color, ColorProps } from 'styled-system'
 
-type TextProps = TypographyProps & SpaceProps
+type TextProps = TypographyProps & SpaceProps & ColorProps;
 
 export const Text = styled.p<TextProps>`
     font-family: 14px;
     color: #3f3f3f;
+    ${color}
     ${typography}
     ${space}
 `
