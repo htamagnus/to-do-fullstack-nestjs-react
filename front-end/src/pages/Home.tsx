adc3118b014fa9758f3603ef96cc31fd5a80efed
Ágatha <agathamagnusg@hotmail.com>
2024-02-03 14:17:10 -0300

    ♻️ Refactor Home component

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,31 +1,61 @@
-import { Input, Text, Button, Row, Column, List, Logo } from 'components'
-import { useState } from 'react'
+import { Input, Text, Button, Row, Column, List, Logo } from "components";
+import { useState } from "react";
 
 export const Home = () => {
-    const [taskName, setTaskName] = useState('');
-    const [tasks, setTasks] = useState<{label: string }[]>([])
+  const [taskName, setTaskName] = useState("");
+  const [tasks, setTasks] = useState<{ label: string }[]>([]);
 
-    const handleOKButton = () => {
-        if (!taskName) return;
-        setTasks(previous => {
-            const copy = [...previous];
-            copy.push({ label: taskName });
-            return copy;
-        })
-        setTaskName('');
-    }
+  const handleOKButton = () => {
+    if (!taskName) return;
+    setTasks((previous) => {
+      const copy = [...previous];
+      copy.push({ label: taskName });
+      return copy;
+    });
+    setTaskName("");
+  };
 
-    return (
-        <Column width="600px" margin="140px auto" background="rgba(255, 255, 255, 0.178)" boxShadow="0 2px 82px 0 rgba(0, 0, 0, 0.5)" borderRadius="12px" paddingBottom="40px" paddingTop="40px">
-            <Column  width="100%" py="10px" alignItems="center">
-                <Logo />
-            </Column>
-            <Text fontWeight="bold" fontSize="bodyLarge" my="10px" pl="10px">Tasks</Text>
-            <Row width="50%">
-            <Input flex={1} placeholder="Enter a task name here.." value={taskName}onChange={(e) => setTaskName(e.target.value)} />
-            <Button onClick={handleOKButton}>Ok</Button>
-            </Row>
-            <List items={tasks} />
-        </Column>
-    )
-}
+  return (
+    <Column
+      width="600px"
+      margin="140px auto"
+      background="rgba(255, 255, 255, 0.178)"
+      boxShadow="0 2px 82px 0 rgba(0, 0, 0, 0.5)"
+      borderRadius="12px"
+      paddingBottom="40px"
+      paddingTop="40px"
+    >
+      <Column width="100%" py="10px" alignItems="center">
+        <Logo />
+      </Column>
+      <Column
+        width="100%"
+        minHeight="300px"
+        bg="rgba(255, 255, 255, 0.2)"
+        borderRadius="4px"
+        p="20px"
+      >
+        <Text fontSize="bodyLarge">Ready</Text>
+        <Text fontSize="displayExtraLarge" py="20px">25:00</Text>
+        <Button variant="primary">
+          <Text fontSize="bodyLarge" color="primary">
+            Start
+          </Text>
+        </Button>
+      </Column>
+      <Text fontWeight="bold" fontSize="bodyLarge" my="10px" pl="10px">
+        Tasks
+      </Text>
+      <Row width="50%">
+        <Input
+          flex={1}
+          placeholder="Enter a task name here.."
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <Button onClick={handleOKButton}>Ok</Button>
+      </Row>
+      <List items={tasks} />
+    </Column>
+  );
+};
