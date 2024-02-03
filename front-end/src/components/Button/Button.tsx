c92c3ba6b104d3a78b40712907d8e2a82bc7c113
Ágatha <agathamagnusg@hotmail.com>
2024-02-03 14:16:50 -0300

    ✨ Add differents variants to Button

--- front-end/src/components/Button/Button.tsx
+++ front-end/src/components/Button/Button.tsx
@@ -1,9 +1,31 @@
 import styled from 'styled-components'
+import {variant} from 'styled-system'
 
-export const Button = styled.button`
+type ButtonProps = {
+    variant?: string
+}
+
+export const Button = styled.button<ButtonProps>`
     padding: 10px 20px;
-    background-color: transparent;
     cursor: pointer;
-    color: #3f3f3f;
-    border-bottom: 2px solid #3f3f3f;
+
+    ${variant({
+        variants: {
+            default: {
+               backgroundColor: 'transparent',
+                color: '#3f3f3f',
+                borderBottom: '2px solid #3f3f3f'
+            },
+            primary: {
+               padding: '10px 20px',
+               backgroundColor: '#3f3f3f',
+               borderRadius: '4px',
+               boxShadow: '0 2px 82px 0 rgba(0, 0, 0, 0.5)',
+            }
+        }
+    })}
 `
+
+Button.defaultProps = {
+    variant: 'default'
+}
