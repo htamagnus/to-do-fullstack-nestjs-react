0dd727491bcfcb11148f8d48a4786a65fdd22ed1
htamagnus <agathamagnusg@hotmail.com>
2024-02-12 11:17:58 -0300

    ✨ Add CreateTodo
    This commit refactors the useTodo hook and TodoService to improve code readability and maintainability. The getAll function in useTodo is renamed to getAllTodos for clarity. Additionally, a new createTodo function is added to TodoService to handle the creation of new todos.

--- front-end/src/hooks/useTodo.tsx
+++ front-end/src/hooks/useTodo.tsx
@@ -6,16 +6,23 @@ import { TodoService } from 'services';
 export const useTodo = () => {
   const [tasks, setTasks] = useState<ITodo[]>([]);
 
-  const getAll = useCallback(async () => {
-    const { status, data } = await TodoService.getAll();
+  const getAllTodos = useCallback(async () => {
+    const { status, data } = await TodoService.getAllTodos();
 
     if (status !== 200) throw new Error();
 
     setTasks(data);
   }, []);
 
+  const createTodo = useCallback(async (todo: Pick<ITodo, 'task' | 'isDone'>) => {
+    const { status } = await TodoService.createTodo(todo);
+
+    if (status !== 201) throw new Error();
+  }, []);
+
   return {
     tasks,
-    getAll
+    getAllTodos, 
+    createTodo
   };
 };
--- front-end/src/services/TodoService.ts
+++ front-end/src/services/TodoService.ts
@@ -1,8 +1,11 @@
 import { ITodo } from "interfaces"
 import { Api } from "providers"
 
-const getAll = () => Api.get<ITodo[]>('/v1/todos')
+const getAllTodos = () => Api.get<ITodo[]>('/v1/todos')
+
+const createTodo = (todo: Pick<ITodo, 'task' | 'isDone'>) => Api.post<ITodo>('/v1/todos', todo)
 
 export const TodoService = {
-    getAll
+    getAllTodos, 
+    createTodo
 }
