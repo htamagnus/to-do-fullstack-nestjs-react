99fdbee1255311e258758eb640fcc411c771486d
htamagnus <agathamagnusg@hotmail.com>
2024-02-12 11:24:32 -0300

    💄 Refactor ListItem component styling

--- front-end/src/components/List/ListItem.tsx
+++ front-end/src/components/List/ListItem.tsx
@@ -11,7 +11,7 @@ export type ListItemProps = {
 
 export const ListItem: React.FC<ListItemProps> = ({ index, id, task, isDone, isActive, onClick }) => {
     return (
-        <Column width="400px" bg="rgba(128, 128, 128, 0.301)" p="20px" my= "4px" mb="10px" borderLeft="5px solid #ffffffd6" borderRadius="4px" cursor= "pointer" borderLeftColor={isActive ? '#fff' : 'transparent'} onClick={() => onClick(index)}>
+        <Column width="400px" bg="rgba(128, 128, 128, 0.301)" p="20px" my= "4px" mb="10px"  borderRadius="4px" cursor= "pointer" borderLeftWidth="5px"  borderLeftStyle="solid" borderLeftColor={isActive ? '#ffffffd6' : 'transparent'} onClick={() => onClick(index)}>
         <Row>
         <Text flex={1}>{task}</Text>
         {isDone === 1 && <Icon variant="done" />}
