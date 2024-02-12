e49353e221ef864b7a86dc9723cba2a594c65097
htamagnus <agathamagnusg@hotmail.com>
2024-02-12 11:57:17 -0300

    ✨ Update ListItem component to add margin to the task text

--- front-end/src/components/List/ListItem.tsx
+++ front-end/src/components/List/ListItem.tsx
@@ -13,7 +13,7 @@ export const ListItem: React.FC<ListItemProps> = ({ index, id, task, isDone, isA
     return (
         <Column width="400px" bg="rgba(128, 128, 128, 0.301)" p="20px" my= "4px" mb="10px"  borderRadius="4px" cursor= "pointer" borderLeftWidth="5px"  borderLeftStyle="solid" borderLeftColor={isActive ? '#ffffffd6' : 'transparent'} onClick={() => onClick(index)}>
         <Row>
-        <Text flex={1}>{task}</Text>
+        <Text mx="80px" flex={1}>{task}</Text>
         {isDone === 1 && <Icon variant="done" />}
         </Row>
         </Column>
