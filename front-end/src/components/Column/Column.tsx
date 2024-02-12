4aa366e7dcbb80aad4b1110607eed1c19165be77
htamagnus <agathamagnusg@hotmail.com>
2024-02-12 11:16:47 -0300

    ✨ Add cursor pointer to ListItem component

--- front-end/src/components/Column/Column.tsx
+++ front-end/src/components/Column/Column.tsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 import { layout, LayoutProps, space, SpaceProps, color, ColorProps, borderRadius, BorderRadiusProps, border, BorderProps, flexbox, FlexboxProps, background, BackgroundProps, boxShadow, BoxShadowProps } from 'styled-system'
 
-type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps & FlexboxProps & BackgroundProps & BoxShadowProps;
+type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps & FlexboxProps & BackgroundProps & BoxShadowProps & { cursor?: string };
 
 export const Column = styled.div<ColumnProps>`
     display: flex;
     flex-direction: column;
     align-items: center;
     flex-wrap: wrap;
+    ${({ cursor }) => cursor && `cursor: ${cursor};`}
     ${flexbox}
     ${layout}
     ${space}
